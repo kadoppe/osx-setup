6ea5bc3e5db3e9208b57b8a28694b8abf5059317
kadoppe <kadoppe@me.com>
2014-12-28 14:37:57 +0900

    Create setup script for Homebrew

---
+++ homebrew-setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Updating homebrew recipes..."
+brew update
+
+echo "Updating existing binaries"
+brew upgrade
+
+binaries=(
+  caskroom/cask/brew-cask
+  coreutils
+  homebrew/dupes/apple-gcc42
+  htop
+  homebrew/versions/maven2
+  mysql
+  nodebrew
+  postgresql
+  rbenv
+  reattach-to-user-namespace
+  ruby-build
+  sonar
+  the_silver_searcher
+  tmux
+  z
+  zsh
+)
+
+echo "Installing binaries..."
+brew install ${binaries[@]}
+
+apps=(
+  alfred
+  adobe-creative-cloud
+  bartender
+  caffeine
+  caskroom/homebrew-versions/macvim-kaoriya
+  cyberduck
+  dropbox
+  firefox
+  flip4mac
+  flux
+  google-chrome
+  google-japanese-ime
+  gyazo
+  iterm2
+  minecraft
+  onepassword
+  owncloud
+  pg-commander
+  skype
+  slack
+  soundflower
+  steam
+  virtualbox
+  vagrant
+  vlc
+  wineskin-winery
+  xtrafinder
+)
+
+echo "Installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+brew cleanup
