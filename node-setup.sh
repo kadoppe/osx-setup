77aad9b6e1af865d3ca01c3b11ebf568018be305
kadoppe <kadoppe@me.com>
2015-03-05 20:15:23 +0900

    Install node v0.10.x and v0.11.x

--- node-setup.sh
+++ node-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+nodebrew install-binary 0.10.36
+nodebrew install-binary 0.11.16
 nodebrew install-binary 0.12.0
-nodebrew use 0.12.0
+nodebrew use 0.10.36
 
 npm install -g react-tools
