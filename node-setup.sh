12caba76cdacbc7e428aa540863a6b2ffb6063a4
kadoppe <kadoppe@me.com>
2015-04-11 11:42:49 +0900

    update node version & install express-generator

--- node-setup.sh
+++ node-setup.sh
@@ -2,12 +2,12 @@
 
 nodebrew install-binary 0.10.36
 nodebrew install-binary 0.11.16
-nodebrew install-binary 0.12.0
-nodebrew use 0.10.36
+nodebrew install-binary 0.12.2
+nodebrew use 0.12.2
 
 npm install -g eslint
 npm install -g react-tools
 npm install -g gulp
 npm install -g bower
-npm install -g mean-cli
+npm install -g express-generator
 
