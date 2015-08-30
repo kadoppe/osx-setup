457a3d144fcf6bbb4eafcdcad178b7083fea3bdc
kadoppe <kadoppe@me.com>
2015-08-30 17:17:02 +0900

    Update Node.js to 0.12.7

--- node-setup.sh
+++ node-setup.sh
@@ -2,8 +2,8 @@
 
 nodebrew install-binary 0.10.36
 nodebrew install-binary 0.11.16
-nodebrew install-binary 0.12.2
-nodebrew use 0.12.2
+nodebrew install-binary 0.12.7
+nodebrew use 0.12.7
 
 npm install -g eslint
 npm install -g react-tools
