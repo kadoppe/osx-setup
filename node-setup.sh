9d3b1d33a3225f7b9d27044d0255551640fc0f56
kadoppe <kadoppe@me.com>
2017-02-26 23:27:08 +0900

    Update node to 7.6.0

--- node-setup.sh
+++ node-setup.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-nodebrew install-binary 7.4.0
-nodebrew use 7.4.0
-
-npm install -g eslint
-npm install -g react-tools
-npm install -g gulp
-npm install -g bower
-npm install -g express-generator
-npm install -g webpack
-npm install -g react-native-cli
+nodebrew install-binary 7.6.0
+nodebrew use 7.6.0
