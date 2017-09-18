bfb5bbe8f08cd6994fd8efb03149a13dcad446d6
kadoppe <kadoppe@me.com>
2017-09-18 11:08:32 +0900

    Install fish plugins

--- fish.sh
+++ fish.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env fish
+
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 fisher install omf/theme-bobthefish
 fisher install z
 fisher install 0rax/fish-bd
+fisher install oh-my-fish/plugin-peco
+fisher install yoshiori/fish-peco_select_ghq_repository
+fisher install rbenv
