a734253437ad92a5e8bc6f6134d674fe839d3583
kadoppe <kadoppe@me.com>
2017-09-18 11:12:20 +0900

    make all plugins up to date

--- fish.sh
+++ fish.sh
@@ -2,9 +2,13 @@
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
+echo "Install plugins..."
 fisher install omf/theme-bobthefish
 fisher install z
 fisher install 0rax/fish-bd
 fisher install oh-my-fish/plugin-peco
 fisher install yoshiori/fish-peco_select_ghq_repository
 fisher install rbenv
+
+echo "Update all plugin..."
+fisher up
