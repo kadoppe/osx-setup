6fdfc098766fd7bd725371a68c0f7d49d498a559
kadoppe <kadoppe@me.com>
2017-12-09 20:17:21 +0900

    setup trackpad, dock, mission control and screen settings

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
+# hostname
 sudo scutil --set ComputerName 'kadoppe-mbp.local'
 sudo scutil --set HostName 'kadoppe-mbp.local'
 
+# trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock tilesize -int 36
+
+# mission control
+defaults write com.apple.dock wvous-tl-corner -int 10
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+# screen
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
