76e3eddf0966dd0968dd00e5b27caf08c5e87bcd
kadoppe <kadoppe@me.com>
2021-01-10 21:46:31 +0900

    wsl

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 binaries=(
+  direnv
+  fish
+  golang-go
+  nodejs
+  peco
+  python
+  python-pip-whl
+  python3
+  python3-pip
   ruby
+  ruby-dev
 )
 
 echo "Installing packages.."
-sudo apt install ${binaries[@]}
+sudo apt install -y ${binaries[@]}
 
 echo "Updateing packages.."
 sudo apt update
