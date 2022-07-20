1d670e626c3dd93a4f80d92622168128f5986360
kadoppe <kadoppe@me.com>
2022-07-20 16:26:55 +0900

    change plugin system to vim-plug

--- vim.sh
+++ vim.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
-rm installer.sh
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
