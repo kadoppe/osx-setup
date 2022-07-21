50531ed864fda73b13c738d6ec75818266f54034
kadoppe <kadoppe@me.com>
2022-07-21 09:13:41 +0900

    update nvm version

--- node-setup.sh
+++ node-setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install v14
