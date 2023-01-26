9c0b417f39717da1dc1d657be470e0d19f1135ab
kadoppe <kadoppe@me.com>
2023-01-26 22:37:40 +0900

    install spanner-cli

--- go-setup.sh
+++ go-setup.sh
@@ -4,3 +4,4 @@ go install github.com/x-motemen/ghq@latest
 go install github.com/mattn/efm-langserver@latest
 go install github.com/mattn/memo@latest
 go install github.com/rhysd/vim-startuptime@latest
+go install github.com/cloudspannerecosystem/spanner-cli@latest
