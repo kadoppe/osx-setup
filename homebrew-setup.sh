a8ad9727bb8ba1cc00a27620e1808527d415ef94
kadoppe <kadoppe@me.com>
2024-11-23 19:32:34 +0900

    change homebrew url

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -2,7 +2,7 @@
 
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Updating homebrew recipes..."
